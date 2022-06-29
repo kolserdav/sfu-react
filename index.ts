a24cc15fb7c68b4753a4e785bc9cf472192bf58e
kolserdav <serega12101983@gmail.com>
2022-06-29 15:11:22 +0700

    non default

--- index.ts
+++ index.ts
@@ -2,14 +2,11 @@ import Hall from './packages/client/dist/Main.esm';
 import Room from './packages/client/dist/components/Room.esm';
 import createServer from './packages/server/src/main';
 
-const uyem = {
-  server: {
-    createServer,
-  },
-  client: {
-    Hall,
-    Room,
-  },
+export const server = {
+  createServer,
 };
 
-export default uyem;
+export const client = {
+  Hall,
+  Room,
+};
