a1c02c636040e6843bb5edbf078f0caa1ac93da6
kolserdav <serega12101983@gmail.com>
2022-06-30 14:31:18 +0700

    d.ts

---
+++ examples/hello-world/src/App.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import React from 'react';
+import { Uyem } from 'uyem/client';
+import './App.css';
+
+const createRoom = () => {
+  // Do not use the symbol "_" in room address
+  window.location.href = `room-${new Date().getTime()}?uid=1`;
+};
+
+function App() {
+  const isHall = window.location.pathname === '/';
+  return (
+    <div>
+      {isHall ? (
+        <button type="button" onClick={createRoom}>
+          Create room
+        </button>
+      ) : (
+        <Uyem id={window.location.search.replace(/\?uid=/, '')} />
+      )}
+    </div>
+  );
+}
+
+export default App;
