5b0fa9aab1bb3ecefc837a8d6d5a20d1979e487d
kolserdav <serega12101983@gmail.com>
2022-07-04 10:59:09 +0700

    Change name

--- packages/client/src/package/Icons/MicrophoneOffIcon.tsx
+++ packages/client/src/package/Icons/MicrophoneOffIcon.tsx
@@ -1,12 +1,12 @@
 /******************************************************************************************
- * Repository: https://github.com/kolserdav/uyem.git
+ * Repository: https://github.com/kolserdav/react-node-webrtc-sfu.git
  * File name: MicrophoneOffIcon.tsx
  * Author: Sergey Kolmiller
  * Email: <uyem.ru@gmail.com>
  * License: BSD-2-Clause
  * License text: Binary distributions of this software include 'wrtc' and other third-party libraries.
  * Copyright: kolserdav, All rights reserved (c)
- * Create Date: Fri Jul 01 2022 17:09:44 GMT+0700 (Krasnoyarsk Standard Time)
+ * Create Date: Mon Jul 04 2022 10:58:51 GMT+0700 (Krasnoyarsk Standard Time)
  ******************************************************************************************/
 import React from 'react';
 import Icon, { IconProps } from './Icon';
