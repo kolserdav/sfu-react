f51bf4662f81a33fa07ab0f4cdf48e72092bcee8
kolserdav <serega12101983@gmail.com>
2022-07-28 22:12:51 +0700

    paths to server

--- packages/client/src/package/Icons/ScreeenIcon.tsx
+++ packages/client/src/package/Icons/ScreeenIcon.tsx
@@ -1,12 +1,12 @@
 /******************************************************************************************
- * Repository: https://github.com/kolserdav/react-node-webrtc-sfu.git
+ * Repository: https://github.com/kolserdav/webrtc-sfu-node-react.git
  * File name: ScreeenIcon.tsx
  * Author: Sergey Kolmiller
  * Email: <uyem.ru@gmail.com>
  * License: MIT
- * License text: 
+ * License text: Show LICENSE file
  * Copyright: kolserdav, All rights reserved (c)
- * Create Date: Thu Jul 14 2022 16:24:49 GMT+0700 (Krasnoyarsk Standard Time)
+ * Create Date: Thu Jul 28 2022 22:09:23 GMT+0700 (Krasnoyarsk Standard Time)
  ******************************************************************************************/
 import React from 'react';
 import Icon, { IconProps } from './Icon';
