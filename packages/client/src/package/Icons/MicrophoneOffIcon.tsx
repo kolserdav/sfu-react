fa69ffba42abf2c8886999af4556213f10daf198
kolserdav <serega12101983@gmail.com>
2022-07-28 22:25:28 +0700

    rename

--- packages/client/src/package/Icons/MicrophoneOffIcon.tsx
+++ packages/client/src/package/Icons/MicrophoneOffIcon.tsx
@@ -1,12 +1,12 @@
 /******************************************************************************************
- * Repository: https://github.com/kolserdav/webrtc-sfu-node-react.git
+ * Repository: https://github.com/kolserdav/webrtc-sfu-werift-react.git
  * File name: MicrophoneOffIcon.tsx
  * Author: Sergey Kolmiller
  * Email: <uyem.ru@gmail.com>
  * License: MIT
  * License text: Show LICENSE file
  * Copyright: kolserdav, All rights reserved (c)
- * Create Date: Thu Jul 28 2022 22:09:23 GMT+0700 (Krasnoyarsk Standard Time)
+ * Create Date: Thu Jul 28 2022 22:20:46 GMT+0700 (Krasnoyarsk Standard Time)
  ******************************************************************************************/
 import React from 'react';
 import Icon, { IconProps } from './Icon';
--- packages/client/src/setupTests.ts
+++ packages/client/src/setupTests.ts
@@ -1,12 +1,12 @@
 /******************************************************************************************
- * Repository: https://github.com/kolserdav/webrtc-sfu-node-react.git
+ * Repository: https://github.com/kolserdav/webrtc-sfu-werift-react.git
  * File name: setupTests.ts
  * Author: Sergey Kolmiller
  * Email: <uyem.ru@gmail.com>
  * License: MIT
  * License text: Show LICENSE file
  * Copyright: kolserdav, All rights reserved (c)
- * Create Date: Thu Jul 28 2022 22:09:23 GMT+0700 (Krasnoyarsk Standard Time)
+ * Create Date: Thu Jul 28 2022 22:20:46 GMT+0700 (Krasnoyarsk Standard Time)
  ******************************************************************************************/
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
