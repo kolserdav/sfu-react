e0b45adeba984adf37d86e7f5134b425216e6d03
kolserdav <serega12101983@gmail.com>
2022-08-19 10:15:32 +0700

    generate to bin

--- bin/uyem.js
+++ bin/uyem.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-require('../packages/server/dist');
+const { spawn } = require('child_process');
+const path = require('path');
+(async () => {
+  console.log('Running "npm run postinstall-server" command...');
+  const res = spawn('npm', ['run', 'postinstall-server'], {
+    env: process.env,
+    cwd: path.resolve(__dirname, '../'),
+  });
+  res.stdout.on('data', (d) => {
+    console.log(d.toString());
+  });
+  res.stderr.on('data', (d) => {
+    console.error(d.toString());
+  });
+  await new Promise((resolve) => {
+    res.on('exit', (e) => {
+      console.log('Command "npm run postinstall-server" exit with code:', e);
+      resolve(0);
+    });
+  });
+  require('../packages/server/dist');
+})();
