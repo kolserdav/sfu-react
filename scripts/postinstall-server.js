947e08b2d3a1882d8166e4c3e34fdeeb75897a58
kolserdav <serega12101983@gmail.com>
2022-08-19 10:08:36 +0700

    generate on start

--- scripts/postinstall-server.js
+++ scripts/postinstall-server.js
@@ -2,35 +2,19 @@
 const { spawn } = require('child_process');
 
 (async () => {
-  console.log('Running "npm run db:dev" command...');
-  let res = spawn('npm', ['run', 'db:dev'], {
+  console.log('Running "npm run prod:migrate" command...');
+  let res = spawn('npm', ['run', 'prod:migrate'], {
     env: process.env,
   });
   res.stdout.on('data', (d) => {
     console.log(d.toString());
   });
   res.stderr.on('data', (d) => {
-    console.log(d.toString());
+    console.error(d.toString());
   });
   await new Promise((resolve) => {
     res.on('exit', (e) => {
-      console.log('Command "npm run db:dev" exit with code:', e);
-      resolve(0);
-    });
-  });
-  console.log('Running "npm run db:prod" command...');
-  res = spawn('npm', ['run', 'db:prod'], {
-    env: process.env,
-  });
-  res.stdout.on('data', (d) => {
-    console.log(d.toString());
-  });
-  res.stderr.on('data', (d) => {
-    console.log(d.toString());
-  });
-  await new Promise((resolve) => {
-    res.on('exit', (e) => {
-      console.log('Command "npm run db:prod" exit with code:', e);
+      console.log('Command "npm run prod:migrate" exit with code:', e);
       resolve(0);
     });
   });
