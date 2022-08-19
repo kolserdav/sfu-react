5a78ed07618fd0935d51d0ba3e5d660c0c771f4b
kolserdav <serega12101983@gmail.com>
2022-08-19 09:02:34 +0700

    need generate

--- scripts/postinstall-server.js
+++ scripts/postinstall-server.js
@@ -1,32 +1,24 @@
 // @ts-check
 const { spawn } = require('child_process');
-const { resolve } = require('path');
-const { existsSync } = require('fs');
 
-const srcPath = resolve(__dirname, '../packages/client/src/package/types/interfaces.ts');
-const isDev = existsSync(srcPath);
 (async () => {
-  if (isDev) {
-    console.log('Running "npm run db:dev" command...');
-    const res = spawn('npm', ['run', 'db:dev'], {
-      env: process.env,
+  console.log('Running "npm run db:dev" command...');
+  let res = spawn('npm', ['run', 'db:dev'], {
+    env: process.env,
+  });
+  res.stdout.on('data', (d) => {
+    console.log(d.toString());
+  });
+  res.stderr.on('data', (d) => {
+    console.log(d.toString());
+  });
+  await new Promise((resolve) => {
+    res.on('exit', (e) => {
+      resolve(0);
     });
-    res.stdout.on('data', (d) => {
-      console.log(d.toString());
-    });
-    res.stderr.on('data', (d) => {
-      console.log(d.toString());
-    });
-    await new Promise((resolve) => {
-      res.on('exit', (e) => {
-        resolve(0);
-      });
-    });
-  } else {
-    console.log('Skipping "npm run db:dev" command.');
-  }
+  });
   console.log('Running "npm run db:prod" command...');
-  const res = spawn('npm', ['run', 'db:prod'], {
+  res = spawn('npm', ['run', 'db:prod'], {
     env: process.env,
   });
   res.stdout.on('data', (d) => {
