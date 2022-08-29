da3c484750b37d1aac4d075b596e5b94019d47dd
kolserdav <serega12101983@gmail.com>
2022-08-29 17:31:20 +0700

    fixed get uid on next

--- examples/ssr-client/pages/[room].tsx
+++ examples/ssr-client/pages/[room].tsx
@@ -1,29 +1,40 @@
-import { useRouter } from 'next/router';
+import React from 'react';
+import { useRouter, NextRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 
 const Uyem = dynamic(() => import('uyem'));
 
-const Room = () => {
+/**
+ * Check if router.asPath are not contain []
+ * it is optional, you can to get userId an another way
+ */
+const checkRouterIsLoad = (router: NextRouter) => !/\]/.test(router.asPath);
+
+function Room() {
   const router = useRouter();
   return (
-    <Uyem
-      room={{
-        server: 'localhost',
-        port: 3001,
-        iceServers: [
-          {
-            urls: ['stun:127.0.0.1:3478'],
-          },
-          {
-            urls: ['turn:127.0.0.2:3478'],
-            username: 'username',
-            credential: 'password',
-          },
-        ],
-        userId: router.asPath.replace(/.+\?uid=/, '') || '0',
-      }}
-    />
+    <div>
+      {checkRouterIsLoad(router) && (
+        <Uyem
+          room={{
+            server: 'localhost',
+            port: 3001,
+            iceServers: [
+              {
+                urls: ['stun:127.0.0.1:3478'],
+              },
+              {
+                urls: ['turn:127.0.0.2:3478'],
+                username: 'username',
+                credential: 'password',
+              },
+            ],
+            userId: router.asPath.replace(/.+\?uid=/, '') || '0',
+          }}
+        />
+      )}
+    </div>
   );
-};
+}
 
 export default Room;
--- packages/client/pages/[room].tsx
+++ packages/client/pages/[room].tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter, NextRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 
 const Uyem = dynamic(() => import('../package/Main'));
 
+const checkRouterIsLoad = (router: NextRouter) => !/\]/.test(router.asPath);
+
 function Room() {
   const router = useRouter();
   return (
-    <Uyem
-      room={{
-        server: 'localhost',
-        port: 3001,
-        iceServers: [
-          {
-            urls: ['stun:127.0.0.1:3478'],
-          },
-          {
-            urls: ['turn:127.0.0.2:3478'],
-            username: 'username',
-            credential: 'password',
-          },
-        ],
-        userId: router.asPath.replace(/.+\?uid=/, '') || '0',
-      }}
-    />
+    <div>
+      {checkRouterIsLoad(router) && (
+        <Uyem
+          room={{
+            server: 'localhost',
+            port: 3001,
+            iceServers: [
+              {
+                urls: ['stun:127.0.0.1:3478'],
+              },
+              {
+                urls: ['turn:127.0.0.2:3478'],
+                username: 'username',
+                credential: 'password',
+              },
+            ],
+            userId: router.asPath.replace(/.+\?uid=/, '') || '0',
+          }}
+        />
+      )}
+    </div>
   );
 }
 
