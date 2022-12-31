9a6f51cf8005cc06cb295a0dd63c6b78519139c5
kolserdav <serega12101983@gmail.com>
2022-12-31 14:24:37 +0700

    create raw video episodes

--- packages/server/src/utils/ffmpeg.ts
+++ packages/server/src/utils/ffmpeg.ts
@@ -12,7 +12,7 @@ import { EXT_WEBM } from './constants';
 // FIXME remove it
 process.env.LOG_LEVEL = '0';
 
-interface VideoChunk {
+interface Chunk {
   id: string;
   start: number;
   end: number;
@@ -21,10 +21,20 @@ interface VideoChunk {
   absPath: string;
 }
 
+type ChunkPart = Chunk & { from: number; to: number };
+
+interface Episode {
+  start: number;
+  end: number;
+  chunks: ChunkPart[];
+}
+
 class Ffmpeg {
   private rtc: RTC;
 
   private videoSrc: string;
+
+  private chunks: Chunk[];
 
   private readonly inputOption = '-i';
 
@@ -43,20 +53,19 @@ class Ffmpeg {
   constructor({ rtc, videoSrc }: { rtc: RTC; videoSrc: string }) {
     this.rtc = rtc;
     this.videoSrc = videoSrc;
+    const dir = fs.readdirSync(this.videoSrc);
+    this.chunks = this.createVideoChunks({ dir });
   }
 
   public async createVideo() {
-    const dir = fs.readdirSync(this.videoSrc);
-    // TODO create args
-    const chunks = this.createVideoChunks({ dir });
-    const inputArgs = this.createInputArguments({ chunks });
-    const filterComplexArgs = this.createFilterComplexArguments({ chunks });
-    console.log(inputArgs.concat(filterComplexArgs), chunks);
+    const inputArgs = this.createInputArguments();
+    const filterComplexArgs = this.createFilterComplexArguments();
+    // console.log(inputArgs.concat(filterComplexArgs), this.chunks);
     // await this.runFfmpegCommand(['--help']);
   }
 
   private createVideoChunks({ dir }: { dir: string[] }) {
-    const chunks: VideoChunk[] = [];
+    const chunks: Chunk[] = [];
     dir.forEach((item) => {
       const peer = item.replace(EXT_WEBM, '').split(this.rtc.delimiter);
       const start = parseInt(peer[0], 10);
@@ -86,21 +95,95 @@ class Ffmpeg {
     });
   }
 
-  private createInputArguments({ chunks }: { chunks: VideoChunk[] }) {
+  private createInputArguments() {
     const args: string[] = [];
-    chunks.forEach((item) => {
+    this.chunks.forEach((item) => {
       args.push(this.inputOption);
       args.push(item.absPath);
     });
     return args;
   }
 
-  private createFilterComplexArguments({ chunks }: { chunks: VideoChunk[] }) {
+  private createFilterComplexArguments() {
     const args = [this.filterComplexOption];
-    chunks.forEach((item) => {
+    const episodes = this.createEpisodes();
+    episodes.forEach((item) => {
+      console.log(item);
+    });
+    this.chunks.forEach((item) => {
       //
     });
     return args;
+  }
+
+  private createEpisodes() {
+    const episodes: Episode[] = [];
+    const time = this.getVideoTime();
+    let oldChunks: Chunk[] = [];
+    let from: number | undefined;
+    new Array(time).fill('').forEach((_, index) => {
+      if (from === undefined) {
+        from = index;
+      }
+      const chunks: Chunk[] = [];
+      this.chunks.every((item) => {
+        if (item.start > index || item.end < index) {
+          return false;
+        }
+        if (item.start <= index && item.end > index) {
+          chunks.push(item);
+        }
+        return true;
+      });
+      oldChunks = oldChunks.length === 0 ? chunks : oldChunks;
+      if (!this.isEqual(chunks, oldChunks)) {
+        const chunkPart: ChunkPart[] = oldChunks.map((item) => {
+          const _item: ChunkPart = { ...item } as any;
+          _item.from = from || 0;
+          _item.to = index;
+          return _item;
+        });
+
+        episodes.push({
+          start: from,
+          end: index,
+          chunks: chunkPart,
+        });
+        from = index;
+      }
+      oldChunks = chunks;
+    });
+    return episodes;
+  }
+
+  private isEqual(a: any[], b: any[]) {
+    let check = true;
+    if (a.length !== b.length) {
+      return false;
+    }
+    a.every((item, index) => {
+      const aKeys = Object.keys(item);
+      aKeys.every((_item) => {
+        if (item[_item] !== b[index]?.[_item]) {
+          check = false;
+          return false;
+        }
+        return true;
+      });
+      return check;
+    });
+    return check;
+  }
+
+  private getVideoTime() {
+    const min = this.chunks[0]?.start || 0;
+    let max = 0;
+    this.chunks.forEach((item) => {
+      if (item.end > max) {
+        max = item.end;
+      }
+    });
+    return max - min;
   }
 
   private async runFfmpegCommand(args: string[]) {
