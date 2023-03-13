96dd6c32733140e0415f5cf47565ce7e0eb71539
kolserdav <serega12101983@gmail.com>
2023-03-13 13:52:26 +0700

    tungstenite

--- packages/server-rs/src/_ws/mod.rs
+++ packages/server-rs/src/_ws/mod.rs
@@ -1,6 +1,15 @@
 use self::messages::{Any, FromValue, GetLocale, GetUserId, SetUserId};
-
 pub use super::locales::{get_locale, Client, LocaleValue};
+use std::{
+    net::{TcpListener, TcpStream},
+    ops::Deref,
+    thread::spawn,
+};
+use tungstenite::{
+    accept_hdr,
+    handshake::server::{Request, Response},
+    Message, WebSocket,
+};
 use uuid::Uuid;
 pub mod messages;
 
@@ -13,70 +22,82 @@ use std::{
     str::FromStr,
     sync::{Arc, Mutex},
 };
-use ws::{listen, CloseCode, Handler, Message, Result as WSResult, Sender};
+mod collections;
+use collections::Sockets;
 
 #[derive(Debug)]
-pub struct Socket {
+pub struct Socket<'a> {
     pub conn_id: String,
-    pub out: Sender,
-}
-
-impl<'a> Handler for Socket {
-    fn on_open(&mut self, shake: ws::Handshake) -> WSResult<()> {
-        Ok(())
-    }
-
-    fn on_message(&mut self, msg: Message) -> WSResult<()> {
-        println!("Server got message '{}'. ", msg);
-        self.out.send(msg)
-    }
-
-    fn on_close(&mut self, code: CloseCode, reason: &str) {
-        println!("WebSocket closing for ({:?}) {}", code, reason);
-        println!(
-            "Shutting down server after first connection closes.: {:?}",
-            self
-        );
-    }
+    pub ws: &'a mut WebSocket<TcpStream>,
 }
 
 #[derive(Debug)]
-pub struct WS {
-    pub sockets: Vec<Sender>,
+
+pub struct WS<'a> {
+    pub sockets: Vec<Socket<'a>>,
 }
 
-impl<'a> WS {
+impl WS<'static> {
     pub fn new() -> Self {
         Self {
             sockets: Vec::new(),
         }
     }
 
-    pub fn listen_ws(
-        &'a mut self,
-        addr: &str,
-        cb: fn(ws: Arc<Mutex<&mut WS>>, msg: Message, out: Sender, conn_id: Uuid) -> ws::Result<()>,
-    ) {
-        let this = Arc::new(Mutex::new(self));
-        let self_t = this.clone();
+    pub fn listen_ws(self, addr: &str) {
+        let server = TcpListener::bind("127.0.0.1:3012").unwrap();
+        for stream in server.incoming() {
+            spawn(move || {
+                let callback = |req: &Request, mut response: Response| {
+                    debug!("Received a new ws handshake");
+                    debug!("The request's path is: {}", req.uri().path());
+                    debug!("The request's headers are:");
+                    for (ref header, _value) in req.headers() {
+                        debug!("* {}: {:?}", header, _value);
+                    }
 
-        let res = listen(addr, |out| {
-            let conn_id = Uuid::new_v4();
-            Socket {
-                out,
-                conn_id: conn_id.to_string(),
-            }
-        });
-        /*
-        if let Err(e) = res {
-            error!("Error in ws {}", e);
-            let mut self_t = self_t.lock().unwrap();
-            self_t.listen_ws(addr, cb);
+                    let headers = response.headers_mut();
+                    headers.append("MyCustomHeader", ":)".parse().unwrap());
+
+                    Ok(response)
+                };
+                let websocket = accept_hdr(stream.unwrap(), callback).unwrap();
+                loop {
+                    let msg = websocket.read_message().unwrap();
+                    if msg.is_binary() || msg.is_text() {
+                        let conn_id = Uuid::new_v4();
+                        self.handle_ws(msg, conn_id, websocket);
+                    }
+                }
+            });
         }
-        */
     }
 
-    pub fn get_locale(&mut self, msg: Message, out: Sender, conn_id: Uuid) {
+    fn handle_ws(self, msg: Message, conn_id: Uuid, ws: WebSocket<TcpStream>) -> Result<(), ()> {
+        let msg_c = msg.clone();
+        let json = self.parse_message::<Any>(msg);
+        if let Err(e) = json {
+            return Ok(());
+        }
+        let json = json.unwrap();
+        let type_mess = &json.r#type;
+
+        match type_mess {
+            MessageType::GET_LOCALE => {
+                self.get_locale(msg_c, ws, conn_id);
+            }
+            MessageType::GET_USER_ID => {
+                self.get_user_id(msg_c, ws, conn_id);
+            }
+            _ => {
+                warn!("Default case of message: {:?}", json);
+            }
+        };
+
+        Ok(())
+    }
+
+    pub fn get_locale(&mut self, msg: Message, ws: WebSocket<TcpStream>, conn_id: Uuid) {
         let msg = self.parse_message::<GetLocale>(msg).unwrap();
         let locale = get_locale(msg.data.locale);
         let mess = MessageArgs {
@@ -87,10 +108,12 @@ impl<'a> WS {
             },
             r#type: MessageType::SET_LOCALE,
         };
-        out.send(to_string(&mess).unwrap()).unwrap();
+        let mut ws = ws;
+        ws.write_message(Message::Text(to_string(&mess).unwrap()))
+            .unwrap();
     }
 
-    pub fn get_user_id(&mut self, msg: Message, out: Sender, conn_id: Uuid) {
+    pub fn get_user_id(&mut self, msg: Message, ws: WebSocket<TcpStream>, conn_id: Uuid) {
         let msg = self.parse_message::<GetUserId>(msg).unwrap();
         let conn_id_str = conn_id.to_string();
 
@@ -111,13 +134,14 @@ impl<'a> WS {
     where
         T: Serialize + Debug,
     {
-        //let out = self.sockets.get(&msg.connId);
-        //if let None = out {
-        //    return Err(());
-        //  }
-        //let out = out.unwrap();
+        let out = self.get_socket_by_id(&msg.connId);
+        if let None = out {
+            return Err(());
+        }
+        let mut out = out.unwrap();
         debug!("Send message: {:?}", &msg);
-        // out.send(to_string(&msg).unwrap()).unwrap();
+        out.write_message(Message::Text(to_string(&msg).unwrap()))
+            .unwrap();
         Ok(())
     }
 
@@ -125,8 +149,8 @@ impl<'a> WS {
     where
         T: FromValue + Debug,
     {
-        let msg_str = msg.as_text().unwrap();
-        let json: serde_json::Value = serde_json::from_str(msg_str).map_err(|err| {
+        let msg_str = msg.to_string();
+        let json: serde_json::Value = serde_json::from_str(msg_str.as_str()).map_err(|err| {
             error!("Failed parse JSON: {:?}", err);
             err
         })?;
@@ -137,5 +161,16 @@ impl<'a> WS {
             r#type: MessageType::from_str(json["type"].as_str().unwrap()).unwrap(),
             data: T::from(&json["data"]),
         })
+    }
+
+    pub fn get_socket_by_id(&mut self, conn_id: &String) -> Option<&mut WebSocket<TcpStream>> {
+        let mut res: Option<&mut WebSocket<TcpStream>> = None;
+        for sock in &mut self.sockets {
+            if sock.conn_id == conn_id.deref() {
+                res = Some(sock.ws);
+                break;
+            }
+        }
+        res
     }
 }
--- packages/server-rs/src/main.rs
+++ packages/server-rs/src/main.rs
@@ -5,41 +5,19 @@ use _ws::{
     messages::{Any, MessageType},
     WS,
 };
-use std::sync::{Arc, Mutex};
+use std::{
+    net::TcpStream,
+    sync::{Arc, Mutex},
+};
 use uuid::Uuid;
-use ws::{Message, Sender};
 mod locales;
 pub mod thread_pool;
+use tungstenite::{Message, WebSocket};
 
 fn main() {
     env_logger::builder().format_timestamp(None).init();
 
     let mut ws = WS::new();
 
-    ws.listen_ws("127.0.0.1:3001", handle_ws);
-}
-
-fn handle_ws(ws: Arc<Mutex<&mut WS>>, msg: Message, out: Sender, conn_id: Uuid) -> ws::Result<()> {
-    let mut ws = ws.lock().unwrap();
-    let msg_c = msg.clone();
-    let json = ws.parse_message::<Any>(msg);
-    if let Err(e) = json {
-        return Ok(());
-    }
-    let json = json.unwrap();
-    let type_mess = &json.r#type;
-
-    match type_mess {
-        MessageType::GET_LOCALE => {
-            ws.get_locale(msg_c, out, conn_id);
-        }
-        MessageType::GET_USER_ID => {
-            ws.get_user_id(msg_c, out, conn_id);
-        }
-        _ => {
-            warn!("Default case of message: {:?}", json);
-        }
-    };
-
-    Ok(())
+    ws.listen_ws("127.0.0.1:3001");
 }
